02f1b3cc4568dc3267c3454feb232156051e4e29
nadezhdaigumenova <nadya.kyzlakova@mail.ru>
2024-06-09 14:29:01 +0700

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,14 @@
+grades = [[5,3,3,5,4],[2,2,2,3,],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+students = {'Johnny','Bilbo','Steve','Khendrik','Aaron'}
+a = (5+3+3+5+4)/5
+b = (2+2+2+3)/4
+c = (4+5+5+2)/4
+d = (4+4+3)/3
+e = (5+5+5+4+5)/5
+one = {'Aaron':a}
+two = {'Bilbo':b}
+three = {'Johnny':c}
+four = {'Steve':d}
+five = {'Khndrik':e}
+students = one, two, three, four, five
+print(students)
