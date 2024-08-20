f758cf649aa551cb35560365adde682d8fd50ee6
nadezhdaigumenova <nadya.kyzlakova@mail.ru>
2024-08-20 17:57:09 +0700

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,14 +1,11 @@
-grades = [[5,3,3,5,4],[2,2,2,3,],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
-students = {'Johnny','Bilbo','Steve','Khendrik','Aaron'}
-a = (5+3+3+5+4)/5
-b = (2+2+2+3)/4
-c = (4+5+5+2)/4
-d = (4+4+3)/3
-e = (5+5+5+4+5)/5
-one = {'Aaron':a}
-two = {'Bilbo':b}
-three = {'Johnny':c}
-four = {'Steve':d}
-five = {'Khndrik':e}
-students = one, two, three, four, five
-print(students)
+grades = [[5,3,3,5,4],[2,2,2,3,],[4,5,5,2],[4,4,],[5,5,5,4,5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_sort = sorted(students)
+grades_all = []
+for i in grades:
+    sum1 = sum(i)/len(i)
+    grades_all.append(sum1)
+dict1 = dict(zip(students_sort, grades_all))
+print(dict1)
+
+
